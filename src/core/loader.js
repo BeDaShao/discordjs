3d6a19006822c67bc37101252b28dcbb506ab6bf
BeDaShao <a0976448885@gmail.com>
2023-12-16 20:43:33 +0800

    [#ep5] face error : can't get files

---
+++ src/core/loader.js
@@ -0,0 +1,53 @@
+/*  #一般的做法
+const updateSlashCommands = () => {
+
+// 註冊斜線指令  使用http請求 --根據discord api
+// 使用axios 框架發送http request -- axios 是ajax方法中的一個框架
+
+axios({
+    method: "PUT",
+    url: "",
+    header: "",
+    date: {},
+});
+};
+-- END 
+*/
+
+import { REST } from "discord.js";
+import { Routes } from "discord.js";
+import dotenv from "dotenv";
+import fg from "fast-glob"; // 快速讀取檔案 --doc. -  https://www.npmjs.com/package/fast-glob -- 已包含在discord.js套件中
+
+dotenv.config();
+
+// rest for rest api
+// discord.js 提供的發送api方法
+
+const updateSlashCommands = () => {
+    const rest = new REST({ version: "10" }).setToken(process.env.TOKEN); // api version: 可以在discord doc 上面查看當前可用的api版本
+    /* # http請求
+    1. full route: 完整的api路徑 -- 利用discord.js提供的routes方法 
+    // -- 其中包含大部分api的路徑，但是仍要帶上參數，例如guild id等等
+    2. options: 額外的選項，這邊帶上指令的資料
+     */
+
+    rest.put(
+        Routes.applicationGuildCommands(
+            // 1. application id, ,2. guild id --建議將guild id以參數帶入
+            process.env.APPLICATION_ID, // application id -- 機器人id
+            "1167406550990717020", //guild id --把"開發者模式"選項打開，在進入你的伺服器(必須是自己的或你有管理員身分)並對伺服器右鍵並點擊"複製伺服器 ID"
+            {
+                // 3. command data -- 利用檔案讀取方式，讀取commands資料夾底下的所有指令
+                body: {},
+            }
+        )
+    );
+};
+
+export const loadCommands = async () => {
+    const files = await fg("./src/commands/**/index.js");
+    console.log(files);
+    // 第一個 ** 表示該資料夾底下所有東西
+    // *.js表其底下所有js檔案 --目前只會用到 index.js
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv"; // 為了從.env檔案中取得環境變數，這個方法需要額外安裝dotenv套件
 import vueInit from "@/core/vue"; // 預設導出(export default)就不用寫大括號，而且物件名稱可以自己取
+import { loadCommands } from "@/core/loader";
 
 vueInit(); // vue.js --pinia
 
@@ -21,3 +22,5 @@ client.once(Events.ClientReady, (readyClient) => {
 client.login(process.env.TOKEN);
 
 // 加一個檔案 ".env" (環境變數) 裡面宣告token
+
+loadCommands();
