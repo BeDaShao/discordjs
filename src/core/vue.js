660fcd643714c4ab1e8421fea826948c7a98bb5d
BeDaShao <a0976448885@gmail.com>
2023-12-16 14:50:11 +0800

    [#ep4] 初始化pinia

--- src/core/vue.js
+++ src/core/vue.js
@@ -1,1 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+// 初始化
+// createApp: 建立一個vue實例
+// pinia 運行在vue底下的插件，所以要先建立vue再建立pinia
+
+export default () => {
+    const vue = createApp();
+    const pinia = createPinia();
+};
