169d3a8c56325132dc9242475c8992c896069c20
BeDaShao <a0976448885@gmail.com>
2023-12-17 08:14:27 +0800

    [#ep5] ep5 ends /  it can run sucessfully, but couldn't get commands

--- src/core/loader.js
+++ src/core/loader.js
@@ -25,24 +25,25 @@ dotenv.config();
 // discord.js 提供的發送api方法
 
 const updateSlashCommands = async (commands) => {
+    // init rest object -- api處理的方法 -- 使用rest api 方法等同於傳送axios請求出去
     const rest = new REST({ version: "10" }).setToken(process.env.TOKEN); // api version: 可以在discord doc 上面查看當前可用的api版本
     /* # http請求
     1. full route: 完整的api路徑 -- 利用discord.js提供的routes方法 
     // -- 其中包含大部分api的路徑，但是仍要帶上參數，例如guild id等等
     2. options: 額外的選項，這邊帶上指令的資料
      */
-
     await rest.put(
         Routes.applicationGuildCommands(
             // 1. application id, ,2. guild id --建議將guild id以參數帶入
             process.env.APPLICATION_ID, // application id -- 機器人id
             "1167406550990717020", //guild id --把"開發者模式"選項打開，在進入你的伺服器(必須是自己的或你有管理員身分)並對伺服器右鍵並點擊"複製伺服器 ID"
             {
-                // 3. command data -- 利用檔案讀取方式，讀取commands資料夾底下的所有指令
-                body: commands,
+                body: commands, // 3. command data (which is option parameter -- depends on what api to use)
+                // -- 利用檔案讀取方式，讀取commands資料夾底下的所有指令
             }
         )
     );
+    console.log("Updated slash commands!");
 };
 
 export const loadCommands = async () => {
@@ -56,8 +57,6 @@ export const loadCommands = async () => {
     for (const file of files) {
         //動態import -- 寫法 import()
         const cmd = await import(file); // cmd for command -- file 代表指令index.js所在路徑
-        //! console.log(cmd); //可以看到引入結果，會對應我們index.js所export出來的物件(command and action) -- 指令外層即其對應的動作
-        //! console.log(cmd.command);  //看command當中的描述
         commands.push(cmd.command);
     }
 
